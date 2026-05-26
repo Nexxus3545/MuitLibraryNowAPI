4b9a346d1020bf6386dcf16c7a921b4742e4cf72
Nexxus3545 <110163589+Nexxus3545@users.noreply.github.com>
2026-05-26 13:36:24 +0800

    Add legacy book ETL import flow

---
+++ MuitLibraryNowAPI/Repositories/InMemoryBookRepository.cs
@@ -0,0 +1,106 @@
+using OleleLibraryNowAPI.Models;
+
+namespace OleleLibraryNowAPI.Repositories
+{
+    public class InMemoryBookRepository : IBookRepository
+    {
+        private readonly List<Book> _books = [];
+        private readonly object _syncRoot = new();
+        private int _nextId = 1;
+
+        public IReadOnlyList<Book> GetAll()
+        {
+            lock (_syncRoot)
+            {
+                return _books
+                    .OrderBy(book => book.Id)
+                    .Select(Clone)
+                    .ToList();
+            }
+        }
+
+        public Book? GetById(int id)
+        {
+            lock (_syncRoot)
+            {
+                var book = _books.FirstOrDefault(existingBook => existingBook.Id == id);
+                return book == null ? null : Clone(book);
+            }
+        }
+
+        public Book Add(Book newBook)
+        {
+            lock (_syncRoot)
+            {
+                var storedBook = Clone(newBook);
+                storedBook.Id = _nextId++;
+                _books.Add(storedBook);
+                return Clone(storedBook);
+            }
+        }
+
+        public Book? Update(int id, Book updatedBook)
+        {
+            lock (_syncRoot)
+            {
+                var existingBook = _books.FirstOrDefault(book => book.Id == id);
+                if (existingBook == null)
+                {
+                    return null;
+                }
+
+                existingBook.Title = updatedBook.Title;
+                existingBook.Author = updatedBook.Author;
+                existingBook.Genre = updatedBook.Genre;
+                existingBook.Available = updatedBook.Available;
+                existingBook.PublishedYear = updatedBook.PublishedYear;
+
+                return Clone(existingBook);
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            lock (_syncRoot)
+            {
+                var book = _books.FirstOrDefault(existingBook => existingBook.Id == id);
+                if (book == null)
+                {
+                    return false;
+                }
+
+                _books.Remove(book);
+                return true;
+            }
+        }
+
+        public void ReplaceAll(IEnumerable<Book> books)
+        {
+            lock (_syncRoot)
+            {
+                _books.Clear();
+                _nextId = 1;
+
+                foreach (var book in books)
+                {
+                    var storedBook = Clone(book);
+                    storedBook.Id = _nextId++;
+                    _books.Add(storedBook);
+                }
+            }
+        }
+
+        private static Book Clone(Book source)
+        {
+            return new Book
+            {
+                Id = source.Id,
+                Title = source.Title,
+                Author = source.Author,
+                Genre = source.Genre,
+                Available = source.Available,
+                PublishedYear = source.PublishedYear
+            };
+        }
+    }
+}
